f82d16b5dd8d8c9d1fcb021b8435864fd3a2f0eb
Kylie <ka-scott@hotmail.co.uk>
2020-12-25 14:46:23 +0000

    function removeTask & clearTasks

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const clearBtn = document.querySelector('.clear-tasks'); // <href> black button 
 const filter = document.querySelector('#filter'); // <input> option at top of tasks list
 const taskList = document.querySelector('.collection'); // <ul> tarts blank/'invisible', appended li appended as user inputs tasks
 
-// Load  event listener
+// Load all  event listeners in one call
 loadEventListeners();
 
 
-// FUNCTION FOR LOADING EVENT LISTENER
+// ADD INDIVIDUAL EVENT LISTENERS
 function loadEventListeners() {
   // input tag within form has 'submit' as type, styled like a button. 'addTask' function called when user clicks on this
   form.addEventListener('submit', addTask)
+  // Event to remove task from list
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 
@@ -23,34 +27,40 @@ function addTask(e) {
   if (taskInput.value === '') {
     alert('Add a task'); // message pops up if user clicks on 'ADD TASK' with blank field
   }
-
   // Create <li> element
   const li = document.createElement('li');
-
-  // Give the li a .class
+  // Give the <li> a .class
   li.className = 'collection-item';
-
-  // Create text node and append to li, pass in whatever use typed in 'New Task' field
+  // Create text node and append to <li>, pass in whatever user typed in 'New Task' field
   li.appendChild(document.createTextNode(taskInput.value));
-
-  // Create new link element to hold  'x'
+  // Create new <a> link element to hold  'x' to delete
   const link = document.createElement('a');
-
-  // Add class
+  // Add .class
   link.className = 'delete-item secondary content';
-
-  // Add icon html
+  // Add <i> icon in html
   link.innerHTML = '<i class="fa  fa-remove"></i>';
-
-  // Append the link to the li
+  // Append the link to the <li>
   li.appendChild(link);
-
-  // Append li to u
+  // Append <li> to <ul>
   taskList.appendChild(li);
-
   // Clear input after it's been appended below
   taskInput.value = '';
 
   e.preventDefault();
+}
 
+// FUNCTION TO REMOVE  TASK
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// FUNCTION TO CLEAR TASKS
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild); // <ul> will remove all children <li>
+  }
 }
